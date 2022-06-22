0777987af569cb99ee0f134834c0e72a05bb9133
CataAg <agr.catalina@gmail.com>
2022-06-22 18:43:23 -0300

    agregado css

--- proyecto_peliculas/catalogo/views/pelicula_views.py
+++ proyecto_peliculas/catalogo/views/pelicula_views.py
@@ -4,9 +4,14 @@ from catalogo.models import Pelicula
 class PeliculaDetailView(DetailView):
     model = Pelicula
     template_name= "pelicula_detail.html"
+
+    def get_object(self):
+        return super().get_object()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        puntaje = self.get_object().puntaje
+        context["puntaje"] = puntaje
         return context
 
 class PeliculaTemplateView(TemplateView):
